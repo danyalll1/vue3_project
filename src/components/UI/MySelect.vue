36cdad875d2d7b773e31914f1747078967ffd02a
danyalll <ne.danyalll@gmail.com>
2023-02-01 16:32:56 +0300

    Create MySelect.vue

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,39 @@
+<template>
+    <select  @change="changeHandler" class="select">
+      <option disabled selected value = ' '>Pick from list</option>
+      <option v-for="option in options "
+              :key="option.value"
+              :value="option.value"
+      >
+        {{option.name}}
+      </option>
+    </select>
+</template>
+
+<script>
+export default {
+  name: "MySelect",
+  props:{
+    modelValue:{
+      type: String
+    },
+    options:{
+      type: Array,
+      default: () => []
+    },
+
+  },
+  methods:{
+    changeHandler(event){
+      this.change=event.target.value
+      this.$emit('update:modelValue',event.target.value)
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .select{
+    margin-left: 10px;
+  }
+</style>
